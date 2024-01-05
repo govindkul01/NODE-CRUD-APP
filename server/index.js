c878a33e31753ddfce8423989c8d7cf083e5fd0f
Govind <govindkulkarni201996@gmail.com>
2024-01-05 22:24:27 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,27 +1,61 @@
-const connection = require('./connection') //connection file
-const express = require('express');
-const bodyParser = require('body-parser');
 
+const express = require("express");
 const app = express();
-const port = 5000;
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 3001;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employeesystem'
+})
+
+//Enable CORS for all routes
+app.use(cors());
 
 //Middleware
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); //
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//GET Method
-app.get('/interns', (req, res) => {
-    const q = "SELECT * FROM intern"
-    connection.query(q, (err, data) => {
-        if (err) return res.json(err)
-        return res.json(data)
+//Create the record
+app.post('/create', (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query("INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+     (err, result) => {
+        if (err){
+            console.log(err);
+        }else{
+            res.send("Data inserted successfully!");
+        }
+    }
+    )
+});
+
+//View the records
+app.get('/employees', (req, res) => {
+    db.query("SELECT * FROM employees", (err, result) => {
+        if (err) {
+        console.log(err);
+        }else{
+            res.send(result);
+        }
+    
     })
 })
 
-//GET by id
-app.get('/interns/:ID', (req, res) => {
-    connection.query('SELECT * FROM INTERN WHERE ID=?', [req.params.ID], (err, rows) => {
+//View the records by id
+
+app.get('/employee/:id', (req, res) => {
+    db.query('SELECT * FROM employees WHERE id=?', [req.params.id], (err, rows) => {
         if (err) {
             console.log(err);
 
@@ -31,55 +65,39 @@ app.get('/interns/:ID', (req, res) => {
     })
 })
 
-//POST method
-app.post("/interns", (req, res) => {
-    const q = "INSERT INTO intern( `Name`, `Email`, `Salary`) VALUES (?)";
-    const values = [
-        req.body.Name,
-        req.body.Email,
-        req.body.Salary,
-    ];
 
-    connection.query(q, [values], (err, data) => {
-        if (err) return res.json(err)
-        return res.json("data added successfully")
-    })
-});
 
-//Update method
-app.put("/intern/:ID", (req, res) => {
+//Update the records by id
 
-    //const q = "UPDATE intern SET `Name`=?, `Email`=?, `Salary`=? WHERE ID=?";
-
-    const uid = req.params.ID;
-    const name = req.body.Name;
-    const email = req.body.Email;
-    const salary = req.body.Salary;
-
-    connection.query('UPDATE intern SET Name=?,Email=?,Salary=? WHERE ID=?', [name, email, salary, uid], (err, result) => {
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query('UPDATE employees SET wage=? WHERE id=?', [wage, id], (err, result) => {
         if (err) {
             console.log(err);
         } else {
-            res.send("Updated data successfully!");
+            res.send(result);
         }
     })
 })
 
-//Delete method
-app.delete('/intern/:ID', (req, res) => {
+//Delete the record from the database
 
-    connection.query('DELETE FROM intern WHERE ID=?', [req.params.ID], (err, rows) => {
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
         if (err) {
             console.log(err);
 
         } else {
-            res.send("Deleted!");
+            res.send(result);
         }
     })
 })
 
 
-//run on port
+//Run on port
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
